c7d4372d82112950b5f3bf2cead8a89b96b7701a
LucaBarbaLata <User@laptop>
2025-06-27 16:03:43 +0300

    Changelog:     * Purged library     * Added a confirmation message that the mod has loaded.

---
+++ src/main/java/net/mcreator/bluemoon/procedures/LoadedMessageProcedure.java
@@ -0,0 +1,12 @@
+package net.mcreator.bluemoon.procedures;
+
+import net.mcreator.bluemoon.BluemoonMod;
+
+public class LoadedMessageProcedure {
+	public static void execute() {
+		BluemoonMod.LOGGER.info("<#00A8FB>&l&nB<#24B4FC>&l&nl<#49C1FC>&l&nu<#6DCDFD>&l&ne<#92DAFD>&l&nM<#B6E6FE>&l&no<#DBF3FE>&l&no<#FFFFFF>&l&nn");
+		BluemoonMod.LOGGER.info(
+				"<#9119FB>P<#9623FB>l<#9B2DFB>u<#9F37FC>g<#A441FC>i<#A94BFC>n <#AE55FC>L<#B25FFC>o<#B769FC>a<#BC73FD>d<#C17DFD>e<#C687FD>d <#CA91FD>S<#CF9BFD>u<#D4A5FD>c<#D9AFFE>c<#DEB9FE>e<#E2C3FE>s<#E7CDFE>s<#ECD7FE>f<#F1E1FE>u<#F5EBFF>l<#FAF5FF>l<#FFFFFF>y");
+		BluemoonMod.LOGGER.info("<#FB0000>A<#FB1717>u<#FC2E2E>t<#FC4646>h<#FC5D5D>o<#FD7474>r<#FD8B8B>: <#FEA2A2>D<#FEB9B9>a<#FED1D1>v<#FFE8E8>i<#FFFFFF>d");
+	}
+}
